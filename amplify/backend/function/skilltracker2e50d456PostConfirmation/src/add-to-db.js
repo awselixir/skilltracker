782b38b9298411607f3a6f0f3d46c02837c6c8f2
Justin Coker <justin.coker@outlook.com>
2023-04-20 09:17:44 -0400

    Created function to add Cognito users to Dynamo on confirm

--- amplify/backend/function/skilltracker2e50d456PostConfirmation/src/add-to-db.js
+++ amplify/backend/function/skilltracker2e50d456PostConfirmation/src/add-to-db.js
@@ -1,6 +1,32 @@
 const aws = require('aws-sdk');
 const ddb = new aws.DynamoDB();
 
+const colorNames = [
+  'blue',
+  'grey',
+  'red',
+  'pink',
+  'purple',
+  'deep-purple',
+  'indigo',
+  'light-blue',
+  'cyan',
+  'teal',
+  'green',
+  'light-green',
+  'amber',
+  'orange',
+  'deep-orange',
+];
+const colorNumbers = ['6', '7', '8', '9', '10'];
+
+const createColor = () => {
+  const name = colorNames[Math.floor(Math.random() * colorNames.length)];
+  const number = colorNumbers[Math.floor(Math.random() * colorNumbers.length)];
+
+  return `${name}-${number}`;
+};
+
 /**
  * @type {import('@types/aws-lambda').PostConfirmationTriggerHandler}
  */
@@ -18,6 +44,7 @@ exports.handler = async (event) => {
       createdAt: { S: date.toISOString() },
       updatedAt: { S: date.toISOString() },
       owner: { S: event.request.userAttributes.sub },
+      color: { S: createColor() },
     },
     TableName: process.env.API_SKILLTRACKER_USERTABLE_NAME,
   };
@@ -25,7 +52,7 @@ exports.handler = async (event) => {
   await ddb.putItem(params).promise();
 
   console.log('Success: Everything executed correctly');
-  console.log(event.request.userAttributes)
+  console.log(event.request.userAttributes);
 
   return event;
 };
