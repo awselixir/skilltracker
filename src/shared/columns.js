6523c1ce1c7a594011f9df6358b1ce5d60f2c15a
Justin Coker <justin.coker@outlook.com>
2023-05-01 22:45:49 -0400

    Skill columns

--- src/shared/columns.js
+++ src/shared/columns.js
@@ -35,6 +35,41 @@ export const certsPageColumns = [
   },
 ];
 
+export const skillsPageColumns = [
+  {
+    name: 'name',
+    field: 'name',
+    label: 'Name',
+    required: true,
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'provider',
+    field: (row) => row.provider.shortName,
+    label: 'Provider',
+    required: true,
+    align: 'left',
+    sortable: true,
+  },
+  // {
+  //   name: 'level',
+  //   field: (row) => row.certificationLevel.name,
+  //   label: 'Level',
+  //   required: true,
+  //   align: 'left',
+  //   sortable: true,
+  // },
+  // {
+  //   name: 'earned',
+  //   field: (row) => (row.users.items.length > 0 ? row.users.items.length : '-'),
+  //   label: 'Earned',
+  //   required: true,
+  //   align: 'center',
+  //   sortable: true,
+  // },
+];
+
 export const usersPageColumns = [
   {
     name: 'name',
