49f92b38c63fc30f39f4ea7af414b2c5328913e7
Justin Coker <justin.coker@outlook.com>
2023-05-05 22:08:23 -0400

    Changed team scores to include skills

--- src/shared/functions.js
+++ src/shared/functions.js
@@ -1,3 +1,6 @@
+import { useSkillStore } from 'src/stores/skill-store';
+const skillStore = useSkillStore();
+
 const colorNames = [
   'blue',
   'grey',
@@ -37,13 +40,46 @@ export const calculateTeamsSkills = (row) => {
 };
 
 export const calculateTeamsScore = (row) => {
-  const usersCerts = []
+  const usersCerts = [];
+  const usersSkills = [];
+
   for (const item of row.users.items) {
     for (const userCert of item.user.certifications.items) {
-      usersCerts.push(userCert)
+      usersCerts.push(userCert);
+    }
+
+    for (const userSkill of item.user.skills.items) {
+      usersSkills.push(userSkill);
     }
   }
-  return usersCerts.reduce((acc, userCert) => {
-    return acc + userCert.certification.certificationLevel.score
-  }, 0)
+  const usersCertsScore = usersCerts.reduce((acc, userCert) => {
+    return acc + userCert.certification.certificationLevel.score;
+  }, 0);
+
+  const usersSkillsScore = usersSkills.reduce((acc, userSkill) => {
+    return acc + skillStore.skillsScores[userSkill.level];
+  }, 0);
+
+  return usersCertsScore + usersSkillsScore;
+};
+
+export const calculateUserScore = (row) => {
+  let certScore = 0;
+  let skillScore = 0;
+
+  if (row.certifications.items.length > 0) {
+    certScore = row.certifications.items.reduce(
+      (acc, cert) => acc + cert.certification.certificationLevel.score,
+      0
+    );
+  }
+
+  if (row.skills.items.length > 0) {
+    skillScore = row.skills.items.reduce(
+      (acc, skill) => acc + skillStore.skillsScores[skill.level],
+      0
+    );
+  }
+
+  return certScore + skillScore;
 };
